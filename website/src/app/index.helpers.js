677ea3548ac3319d8f560c5afc6ae13389534e75
Kalarrs Topham <k-dawg-klark@hotmail.com>
2015-10-11 09:36:09 -0700

    * showing login button in main nav * showing error on login * moved header and footer into templates * placed avatar in the nav

--- website/src/app/index.helpers.js
+++ website/src/app/index.helpers.js
@@ -1,59 +1,60 @@
-	(function () {
-	'use strict';
-	
-	angular
-		.module('website')
-		.service('authHelper', authHelper);
-	
-	/** @ngInject */
-	function authHelper($rootScope, $q, userCache, userHttpCache, chandlerFoodBankApi) {
-		var storeSession = function (res) {
-		try {
-			userCache.put('user', res.data);
-			userCache.put('jwt', res.meta.jwt);
-			$rootScope.user = res.data;
-			return null;
-		} catch (err) {
-			return $q.reject('unable to save the user to local storage!');
-		}
-		};
-	
-		this.login = function (data) {
-		this.logout();
-		return chandlerFoodBankApi.login(data).then(storeSession);
-		};
-	
-		this.registerAndLogin = function (data) {
-		this.logout();
-		return chandlerFoodBankApi.register(data).then(storeSession);
-		};
-	
-		this.logout = function () {
-		// Note don't clear global caches, as they don't contain user specific data.
-		userHttpCache.removeAll();
-		userCache.removeAll();
-		};
-	}
-	
-	angular
-		.module('website')
-		.service('eventsHelper', eventsHelper);
-	
-	/** @ngInject */
-	function eventsHelper() {
-		this.eventsToCalendarEvents = function (events) {
-	
-		var calendarEvents = [];
-	
-		for (var i in events) {
-			calendarEvents.push({
-			title: events[i].name || null,
-			start: events[i].date && events[i].date.start || null,
-			end: events[i].date && events[i].date.end || null
-			});
-		}
-	
-		return calendarEvents;
-		};
-	}
-	})();
+(function () {
+  'use strict';
+
+  angular
+    .module('website')
+    .service('authHelper', authHelper);
+
+  /** @ngInject */
+  function authHelper($rootScope, $q, userCache, userHttpCache, chandlerFoodBankApi) {
+    var storeSession = function (res) {
+      try {
+        userCache.put('user', res.data);
+        userCache.put('jwt', res.meta.jwt);
+        $rootScope.user = res.data;
+        return null;
+      } catch (err) {
+        return $q.reject('unable to save the user to local storage!');
+      }
+    };
+
+    this.login = function (data) {
+      this.logout();
+      return chandlerFoodBankApi.login(data).then(storeSession);
+    };
+
+    this.registerAndLogin = function (data) {
+      this.logout();
+      return chandlerFoodBankApi.register(data).then(storeSession);
+    };
+
+    this.logout = function () {
+      // Note don't clear global caches, as they don't contain user specific data.
+      userHttpCache.removeAll();
+      userCache.removeAll();
+      $rootScope.user = null;
+    };
+  }
+
+  angular
+    .module('website')
+    .service('eventsHelper', eventsHelper);
+
+  /** @ngInject */
+  function eventsHelper() {
+    this.eventsToCalendarEvents = function (events) {
+
+      var calendarEvents = [];
+
+      for (var i in events) {
+        calendarEvents.push({
+          title: events[i].name || null,
+          start: events[i].date && events[i].date.start || null,
+          end: events[i].date && events[i].date.end || null
+        });
+      }
+
+      return calendarEvents;
+    };
+  }
+})();
