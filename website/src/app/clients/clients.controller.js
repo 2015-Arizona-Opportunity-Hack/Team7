27b9efca34ec2bacfdaadf7e7257086818c1d1d6
rbradhill <rbradhill@gmail.com>
2015-10-11 03:11:25 -0700

    clients form

---
+++ website/src/app/clients/clients.controller.js
@@ -0,0 +1,27 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('website')
+    .controller('ClientsController', ClientsController);
+
+  /** @ngInject */
+  function ClientsController(calendarEvents) {
+    var vm = this;
+
+    vm.calendarConfig = {
+      calendar:{
+        height: 450,
+        editable: true,
+        header:{
+          left: 'month basicWeek basicDay agendaWeek agendaDay',
+          center: 'title',
+          right: 'today prev,next'
+        }
+      }
+    };
+
+    vm.allEvents = [calendarEvents];
+
+  }
+})();
