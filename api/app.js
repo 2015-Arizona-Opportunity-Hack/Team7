8c56cd5d94b4f4ef673f5c46173f584880b8dd5d
Kalarrs Topham <k-dawg-klark@hotmail.com>
2015-10-11 13:49:56 -0700

    * heroku

--- api/app.js
+++ api/app.js
@@ -24,18 +24,16 @@ var app = module.exports = exports.app = express();
 
 app.set('showStackError', true);
 
-
-
 // Should be placed before express.static
 // To ensure that all assets and data are compressed (utilize bandwidth)
-//app.use(compression({
-//  filter: function(req, res) {
-//    return (/json|text|javascript|css/).test(res.getHeader('Content-Type'));
-//  },
-//  // Levels are specified in a range of 0 to 9, where-as 0 is
-//  // no compression and 9 is best compression, but slowest
-//  level: 4
-//}));
+app.use(compression({
+  filter: function(req, res) {
+    return (/json|text|javascript|css/).test(res.getHeader('Content-Type'));
+  },
+  // Levels are specified in a range of 0 to 9, where-as 0 is
+  // no compression and 9 is best compression, but slowest
+  level: 4
+}));
 
 // Bootstrap models
 var modelsPath = path.join(srcPath, 'models');
@@ -45,85 +43,75 @@ models.forEach(function (file) {
 });
 
 
-//if ('development' == env) {
-//  app.use(morgan('dev'));
-//  app.use(errorhandler({
-//    dumpExceptions: true,
-//    showStack: true
-//  }));
-//  app.set('view options', {
-//    pretty: true
-//  });
-//}
-//
-//if ('test' == env) {
-//  app.use(morgan('dev'));
-//  app.set('view options', {
-//    pretty: true
-//  });
-//  app.use(errorhandler({
-//    dumpExceptions: true,
-//    showStack: true
-//  }));
-//}
-//
-//if ('production' == env) {
-//  app.use(morgan());
-//  app.use(errorhandler({
-//    dumpExceptions: false,
-//    showStack: false
-//  }));
-//}
-//
-//// Prevent clickjacking
-//app.use(helmet.frameguard());
-//
-//// Implement X-XSS-Protection
-//app.use(helmet.xssFilter());
-//
-//// Implement X-Frame: Deny
-//app.use(helmet.xframe());
-//
-//// Keep clients from sniffing the MIME type
-//app.use(helmet.noSniff());
-//
-//// Implement Strict-Transport-Security
-//app.use(helmet.hsts({
-//  maxAge: 7776000000,
-//  includeSubdomains: true
-//}));
-//
-//// Hide X-Powered-By
-//app.use(helmet.hidePoweredBy());
-//
-////CSP protection
-//app.use(helmet.contentSecurityPolicy({
-//  defaultSrc: ["'self'", 'crowdfunding.com'],
-//  reportOnly: false, // set to true if you only want to report errors
-//  setAllHeaders: false, // set to true if you want to set all headers
-//  disableAndroid: false, // set to true if you want to disable Android (browsers can vary and be buggy)
-//  safari5: false // set to true if you want to force buggy CSP in Safari 5
-//}));
+if ('development' == env) {
+  app.use(morgan('dev'));
+  app.use(errorhandler({
+    dumpExceptions: true,
+    showStack: true
+  }));
+  app.set('view options', {
+    pretty: true
+  });
+}
+
+if ('test' == env) {
+  app.use(morgan('dev'));
+  app.set('view options', {
+    pretty: true
+  });
+  app.use(errorhandler({
+    dumpExceptions: true,
+    showStack: true
+  }));
+}
+
+if ('production' == env) {
+  app.use(morgan());
+  app.use(errorhandler({
+    dumpExceptions: false,
+    showStack: false
+  }));
+}
+
+// Prevent clickjacking
+app.use(helmet.frameguard());
+
+// Implement X-XSS-Protection
+app.use(helmet.xssFilter());
+
+// Implement X-Frame: Deny
+app.use(helmet.xframe());
+
+// Keep clients from sniffing the MIME type
+app.use(helmet.noSniff());
+
+// Implement Strict-Transport-Security
+app.use(helmet.hsts({
+  maxAge: 7776000000,
+  includeSubdomains: true
+}));
+
+// Hide X-Powered-By
+app.use(helmet.hidePoweredBy());
+
+//CSP protection
+app.use(helmet.contentSecurityPolicy({
+  defaultSrc: ["'self'", 'crowdfunding.com'],
+  reportOnly: false, // set to true if you only want to report errors
+  setAllHeaders: false, // set to true if you want to set all headers
+  disableAndroid: false, // set to true if you want to disable Android (browsers can vary and be buggy)
+  safari5: false // set to true if you want to force buggy CSP in Safari 5
+}));
 
 
 // Connect to mongo db using mongoose driver
 var mongoose = require(path.join(srcPath, '/config/mongoose'));
-app.use(cors());
-
-app.use(function (req, res, next) {
-  console.log(req.body) // populated!
-  next();
-});
 
 app.use(methodOverride());
-app.use(bodyParser.json({extended:true}));
-app.use(function (req, res, next) {
-  console.log(req.body) // populated!
-  next();
-});
-
-//app.use(expressValidator());
-
+app.use(bodyParser.json());
+app.use(expressValidator());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cors());
 
 // Bootstrap routes/api
 var routesPath = path.join(srcPath, '/routes');
