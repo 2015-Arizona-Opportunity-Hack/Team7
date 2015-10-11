eeda7fa3901a6f6569276ba63a323e6345e0895f
Kalarrs Topham <k-dawg-klark@hotmail.com>
2015-10-10 17:53:05 -0700

    * Adding example of getting events

--- app/www/js/Controllers/GetFoodPersonDetailsController.js
+++ app/www/js/Controllers/GetFoodPersonDetailsController.js
@@ -1,30 +1,29 @@
 angular.module('starter.controller.getfoodperson', [])
-	.controller('GetFoodPersonDetailsController', function ($scope) {
-		
-		console.log("GetFoodPersonDetailsController");
-		var str = "";
-		for (var index = 0; index < 4; index++)
-		{
-			var now = moment().add('w', index),
-				begin = now.startOf('isoweek');			
+  .controller('GetFoodPersonDetailsController', function ($scope) {
 
-				str += "<div class='item'><h3>" + begin.format('M-D-YYYY') 
-				var end = begin.add('d', 4);
-				
-				str += ' to ' + begin.format('M-D-YYYY') + '</h3>';
-				str += "<div>";
-				str += '<div class="button-bar button-bar-inline"><a class="button button-large button-balanced">M</a><a class="button button-large">Tu</a><a class="button button-large">W</a><a class="button button-large">Th</a></div>';
-				str += '<div class="button-bar button-bar-inline"><a class="button button-large">F</a></div>';
-				str += "</div></div >";
-		}
-		
-		$scope.temp = str
+    console.log("GetFoodPersonDetailsController");
+    var str = "";
+    for (var index = 0; index < 4; index++) {
+      var now = moment().add('w', index),
+        begin = now.startOf('isoweek');
+
+      str += "<div class='item'><h3>" + begin.format('M-D-YYYY')
+      var end = begin.add('d', 4);
+
+      str += ' to ' + begin.format('M-D-YYYY') + '</h3>';
+      str += "<div>";
+      str += '<div class="button-bar button-bar-inline"><a class="button button-large button-balanced">M</a><a class="button button-large">Tu</a><a class="button button-large">W</a><a class="button button-large">Th</a></div>';
+      str += '<div class="button-bar button-bar-inline"><a class="button button-large">F</a></div>';
+      str += "</div></div >";
+    }
+
+    $scope.temp = str
 // document.body.innerHTML = str;
-		
-		
+
+
 // 					<div>
 // 			<h3>Ocotober 12-16</h3>
-// 		</div>		
+// 		</div>
 // 			<div class="button-bar button-small item-radio">
 //   <a class="button button-large button-balanced">M</a>
 //   <a class="button button-large">Tu</a>
@@ -33,6 +32,34 @@ angular.module('starter.controller.getfoodperson', [])
 //   <a class="button button-large">F</a>
 // </div>
 
-					
-		
-	});
+    // /api/events?rollingWeeks=4&type=FOOD
+    var events = [
+      {
+        name: 'Food',
+        type: 'FOOD',
+        date: {
+          start: 'utc 8:00am',
+          end: 'utc 5:00pm'
+        },
+        description: '',
+        picture: '',
+        locations: [
+          {
+            name: 'Walgreens',
+            address1: '',
+            address2: '',
+            city: '',
+            state: '',
+            zip: '',
+            coordinates: ['', ''] // lng, lat
+          }
+        ]
+      }
+    ];
+
+    // using moment create this structure. Rolling 4 week
+    var rollingWeeks = [
+      ['utcDate day 1', 'utcDate day 2'], // week 1
+      [] // week 2
+    ];
+  });
